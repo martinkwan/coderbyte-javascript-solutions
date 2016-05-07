e09af3c4747e88b0c5b40a981310f7488083b4c7
Martin <martinkkwan@gmail.com>
2016-05-07 14:09:01 -0700

    Added secondary solution

--- 01-firstReverse.js
+++ 01-firstReverse.js
@@ -1,7 +1,22 @@
 //Have the function FirstReverse(str) take the str parameter being passed and return the string in reversed order. 
 
+function FirstReverse(str) {
+  //split string into an array of characters
+  return str.split("")     
+  			//reverses the array
+            .reverse()     
+            //joins reversed array into a string
+            .join("");      
+}
+
+
+//secondary solution
 function FirstReverse(str) { 
-  return str.split("")     //split string into an array
-            .reverse()     //reverses array
-            .join("");     //joins array into a string    
+  var newStr = "";
+  //loop through string backwards
+  for(var i= str.length-1; i>=0; i--){	
+    //add characters to newStr in reverse order	
+    newStr += str[i]; 		
+  }
+  return newStr;  								
 }
