409f6b4edd3f7e44d2a33b4800cb001de3070b64
Martin <martinkkwan@gmail.com>
2016-05-07 00:00:49 -0700

    First file commit

---
+++ 01-firstReverse.js
@@ -0,0 +1,10 @@
+//Have the function FirstReverse(str) take the str parameter being passed and return the string in reversed order. 
+
+
+function FirstReverse(str) { 
+  var newStr = "";
+  for(var i= str.length-1; i>=0; i--){
+   newStr += str[i]; 
+  }
+  return newStr;       
+}
